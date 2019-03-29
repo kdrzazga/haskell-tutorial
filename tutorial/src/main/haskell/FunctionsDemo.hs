3c5a0467002ed8edc5fc745f51caf5eebaa776b8
Krzysztof Drzazga <krzysztof.drzazga@capgemini.com>
2019-03-29 16:05:19 +0100

    Demos for functions and tuples Basic commands with comment

--- tutorial/src/main/haskell/FunctionsDemo.hs
+++ tutorial/src/main/haskell/FunctionsDemo.hs
@@ -1,7 +1,14 @@
 module FunctionsDemo where
 
-add :: Integer -> Integer -> Integer --  :: means 'has type'
-add x y = x + y
+--  :: means 'has type'
+add :: Integer -> Integer -> Integer
+
+--Function declaration:
+-- funcName param1 param2 = operations (return value)
+add x y = x + y --works ONLY with Integers
+--funcName CANNOT start with capital letter
+
+sumMe x y  = x + y --works also with Floats
 
 inc x = x + 1
 plus2 y = inc(inc(y))
@@ -24,3 +31,13 @@ describeLetter2 c
    | otherwise            = "Not an ASCII letter"
 
 help = print "Avaialable functions: add 2 3, inc 5, describeLetter '1'"
+
+-- Tuple with Ints and another Tuple with Ints give Tuple with Ints as result
+addTuples :: (Int, Int) -> (Int, Int) -> (Int, Int)
+addTuples (x,y) (x2, y2) = (x +x2, y + y2)
+
+whatAge :: Int -> String
+
+whatAge 7 = "You go to school"
+whatAge 18 = "You can vote"
+whatAge x = "Nothing important"
